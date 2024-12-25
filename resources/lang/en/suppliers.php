d9d711b4ebe2a26d20f4e6847bdba7fd30eaf31f
Khoshbin <xoshbin@gmail.com>
2024-12-25 13:22:26 +0300

    feat(suppliers): add localization support for supplier form and table labels

---
+++ resources/lang/en/suppliers.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'table' => [
+        'name' => 'Name',
+        'contact_person' => 'Contact Person',
+        'email' => 'Email',
+        'phone' => 'Phone',
+        'created_at' => 'Created At',
+        'updated_at' => 'Updated At',
+    ],
+
+    'form' => [
+        'name' => 'Name',
+        'contact_person' => 'Contact Person',
+        'email' => 'Email',
+        'phone' => 'Phone',
+        'address' => 'Address',
+    ],
+];
