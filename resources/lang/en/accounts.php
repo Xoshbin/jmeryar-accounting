c718ccf528df473d8075e149d454d75b5eefc8e7
Khoshbin <xoshbin@gmail.com>
2024-12-25 10:43:40 +0300

    feat(accounts): add localization support for account resource labels and messages

---
+++ resources/lang/en/accounts.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'table' => [
+        'name' => 'Name',
+        'parent_name' => 'Parent Name',
+        'code' => 'Code',
+        'type' => 'Type',
+        'created_at' => 'Created At',
+        'updated_at' => 'Updated At',
+    ],
+
+    'form' => [
+        'name' => 'Name',
+        'type' => 'Type',
+        'asset' => 'Asset',
+        'liability' => 'Liability',
+        'equity' => 'Equity',
+        'revenue' => 'Revenue',
+        'expense' => 'Expense',
+        'code' => 'Code',
+        'parent_id' => 'Parent Account',
+    ],
+];
