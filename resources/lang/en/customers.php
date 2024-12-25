c3cc45afd72cdab92aaec46a1bb3403511e77542
Khoshbin <xoshbin@gmail.com>
2024-12-25 11:40:47 +0300

    feat(customers): add localization support for customer resource labels and messages

---
+++ resources/lang/en/customers.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'customers' => 'Customers',
+    'table' => [
+        'name' => 'Name',
+        'email' => 'Email',
+        'phone' => 'Phone',
+        'created_at' => 'Created At',
+        'updated_at' => 'Updated At',
+    ],
+
+    'form' => [
+        'name' => 'Name',
+        'email' => 'Email',
+        'phone' => 'Phone',
+        'address' => 'Address',
+    ],
+];
