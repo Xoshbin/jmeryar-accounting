a9c8834c21ca51f4e9c2be72801b1edff1febfe9
Khoshbin <xoshbin@gmail.com>
2024-12-28 00:30:49 +0300

    Fix the exchange rate value in InvoiceResource payments

--- src/JmeryarPanel/Resources/InvoiceResource.php
+++ src/JmeryarPanel/Resources/InvoiceResource.php
@@ -338,8 +338,13 @@ class InvoiceResource extends Resource
                                             ->postfix('*')
                                             ->required()
                                             ->afterStateUpdated(function ($state, callable $set, callable $get) {
-                                                $exchange_rate = $get('exchange_rate') ?? 0;
-                                                $set('amount_in_document_currency', $exchange_rate * $state);
+                                                $exchange_rate = $get('exchange_rate') ?? 0; // Exchange rate in IQD per USD
+                                                if ($exchange_rate > 0) {
+                                                    $amount_in_usd = $state / $exchange_rate; // Convert IQD to USD
+                                                    $set('amount_in_document_currency', $amount_in_usd);
+                                                } else {
+                                                    $set('amount_in_document_currency', 0); // Default to 0 if exchange rate is invalid
+                                                }
                                             }),
                                         Forms\Components\TextInput::make('exchange_rate')
                                             ->label(__('jmeryar-accounting::invoices.form.exchange_rate'))
