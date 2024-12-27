0a245180f4480c2cd43d53e8e029630b62a52f5b
Xoshbin <Xoshbin@users.noreply.github.com>
2024-12-27 13:43:22 +0000

    chore: fix code style

--- src/JmeryarPanel/Resources/ProductResource/RelationManagers/BillsRelationManager.php
+++ src/JmeryarPanel/Resources/ProductResource/RelationManagers/BillsRelationManager.php
@@ -6,9 +6,9 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
-use Filament\Tables\Table;
 use Filament\Tables\Columns\Summarizers\Sum;
 use Filament\Tables\Columns\Summarizers\Summarizer;
+use Filament\Tables\Table;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Facades\DB;
 
@@ -20,39 +20,39 @@ class InventoryBatchesRelationManager extends RelationManager
     {
         return
             $form
-            ->schema([
-                Forms\Components\Grid::make(3)
-                    ->schema([
-                        Forms\Components\Section::make()
-                            ->columns(2)
-                            ->schema([
-                                Forms\Components\Select::make('product_id')
-                                    ->label(__('jmeryar-accounting::inventory.form.name'))
-                                    ->relationship('product', 'name')
-                                    ->required(),
-                                Forms\Components\TextInput::make('quantity')
-                                    ->numeric()
-                                    ->label(__('jmeryar-accounting::inventory.form.quantity'))
-                                    ->maxLength(255),
-                                Forms\Components\TextInput::make('cost_price')
-                                    ->label(__('jmeryar-accounting::inventory.form.cost_price'))
-                                    ->numeric()
-                                    ->maxLength(255),
-                                Forms\Components\TextInput::make('unit_price')
-                                    ->label(__('jmeryar-accounting::inventory.form.unit_price'))
-                                    ->numeric(),
-                            ])
-                            ->columnSpan(2),
-                        Forms\Components\Grid::make()
-                            ->schema([
-                                Forms\Components\Section::make()->schema([
-                                    Forms\Components\DatePicker::make('expiry_date')
-                                        ->label(__('jmeryar-accounting::inventory.form.expiry_date')),
-                                ]),
-                            ])
-                            ->columnSpan(1),
-                    ]),
-            ]);
+                ->schema([
+                    Forms\Components\Grid::make(3)
+                        ->schema([
+                            Forms\Components\Section::make()
+                                ->columns(2)
+                                ->schema([
+                                    Forms\Components\Select::make('product_id')
+                                        ->label(__('jmeryar-accounting::inventory.form.name'))
+                                        ->relationship('product', 'name')
+                                        ->required(),
+                                    Forms\Components\TextInput::make('quantity')
+                                        ->numeric()
+                                        ->label(__('jmeryar-accounting::inventory.form.quantity'))
+                                        ->maxLength(255),
+                                    Forms\Components\TextInput::make('cost_price')
+                                        ->label(__('jmeryar-accounting::inventory.form.cost_price'))
+                                        ->numeric()
+                                        ->maxLength(255),
+                                    Forms\Components\TextInput::make('unit_price')
+                                        ->label(__('jmeryar-accounting::inventory.form.unit_price'))
+                                        ->numeric(),
+                                ])
+                                ->columnSpan(2),
+                            Forms\Components\Grid::make()
+                                ->schema([
+                                    Forms\Components\Section::make()->schema([
+                                        Forms\Components\DatePicker::make('expiry_date')
+                                            ->label(__('jmeryar-accounting::inventory.form.expiry_date')),
+                                    ]),
+                                ])
+                                ->columnSpan(1),
+                        ]),
+                ]);
     }
 
     public function table(Table $table): Table
@@ -71,12 +71,12 @@ class InventoryBatchesRelationManager extends RelationManager
                     ->label(__('jmeryar-accounting::inventory.table.cost_price'))
                     ->summarize(Summarizer::make()
                         ->label('Total Cost')
-                        ->using(fn(Builder $query): string => (string) $query->sum(DB::raw('cost_price * quantity')) / 100)),
+                        ->using(fn (Builder $query): string => (string) $query->sum(DB::raw('cost_price * quantity')) / 100)),
                 Tables\Columns\TextColumn::make('unit_price')
                     ->label(__('jmeryar-accounting::inventory.table.unit_price'))
                     ->summarize(Summarizer::make()
                         ->label('Total Value')
-                        ->using(fn(Builder $query): string => (string) $query->sum(DB::raw('unit_price * quantity')) / 100)),
+                        ->using(fn (Builder $query): string => (string) $query->sum(DB::raw('unit_price * quantity')) / 100)),
                 Tables\Columns\TextColumn::make('expiry_date')
                     ->label(__('jmeryar-accounting::inventory.table.expiry_date'))
                     ->searchable(),
